8b693ca7671b831663bd934d7186ffa465a1a8b3
DanielBergshoeff <dabergshoeff@gmail.com>
2018-11-27 19:35:30 +0100

    Skilltool original commit

---
+++ Assets/SkillManager.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkillManager : MonoBehaviour {
+
+    [SerializeField]
+    private Transform startPosition;
+
+    [SerializeField]
+    private List<Vector3> targetPositions;
+
+    [SerializeField]
+    private GameObject prefabSkill;
+    
+    
+
+    //TRIGGER
+    //Trigger to start skill
+    public KeyCode input;
+    public float cooldownTime;
+
+    //Min and max time between skill start at random
+    public float minTime;
+    public float maxTime;
+
+    //Time between skill start -> continuously
+    public float timeBetween;
+    private float currentTimeWaited;
+
+    //POSITION
+    public Vector3 skillPositionVector;
+
+    //Position based on GameObject
+    public GameObject skillPositionObject;
+
+    //Position based on direction and distance
+    public Vector3 skillPositionDirection;
+    public float skillPositionDistance;
+
+    //TARGET
+    public Vector3 skillTargetVector;
+
+    //Target based on GameObject
+    public GameObject skillTargetObject;
+
+    //Target based on direction and distance
+    public Vector3 skillTargetDirection;
+    public float skillTargetDistance;
+
+    public float skillSpeed;
+
+    private List<GameObject> skills;
+
+    [HideInInspector]
+    public int currentTab;
+
+    [HideInInspector]
+    public int triggerChoice, positionChoice, positionChoice1, positionChoice1Direction, targetChoice1, targetChoice1Direction;
+
+    // Use this for initialization
+    void Start() {
+        skills = new List<GameObject>();
+        targetPositions = new List<Vector3>();
+
+        switch (triggerChoice) {
+            case 1:
+                currentTimeWaited = Random.Range(minTime, maxTime);
+                break;
+            case 2:
+                currentTimeWaited = timeBetween;
+                break;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //If trigger by player input
+        if (triggerChoice == 0) {
+            if (Input.GetKeyDown(input)) {
+                InstantiateSkill();
+            }
+        }
+        //If trigger by time
+        else if(triggerChoice == 1 || triggerChoice == 2) {
+            currentTimeWaited -= Time.deltaTime;
+            if(currentTimeWaited <= 0) {
+                InstantiateSkill();
+                //If trigger at random intervals
+                if (triggerChoice == 1)
+                    currentTimeWaited = Random.Range(minTime, maxTime);
+                //If trigger at specific intervals
+                else if (triggerChoice == 2)
+                    currentTimeWaited = timeBetween;
+            }
+        }
+
+        if (positionChoice == 1) {
+            for (int i = 0; i < skills.Count; i++) {
+                skills[i].transform.position = Vector3.MoveTowards(skills[i].transform.position, targetPositions[i], Time.deltaTime * skillSpeed);
+            }
+        }
+	}
+
+    private Vector3 GetPositionFromMenu(int choice1, int choice1dir, Vector3 vec, GameObject go, float dist) {
+        Vector3 positionToSpawn = Vector3.zero;
+
+        switch (choice1) {
+            //If skill position is at a global position
+            case 0:
+                positionToSpawn = vec;
+                break;
+            //If skill position is relative to this local position
+            case 1:
+                positionToSpawn = this.transform.position + vec;
+                break;
+            //If skill position is at a different game object's position
+            case 2:
+                positionToSpawn = go.transform.position;
+                break;
+            //If skill position is in a certain direction
+            case 3:
+                Vector3 directionVector = Vector3.zero;
+                if (dist > 0) {
+                    switch (choice1dir) {
+                        //Forward
+                        case 0:
+                            directionVector = transform.forward;
+                            break;
+                        //Backward
+                        case 1:
+                            directionVector = -transform.forward;
+                            break;
+                        //Left
+                        case 2:
+                            directionVector = -transform.right;
+                            break;
+                        //Right
+                        case 3:
+                            directionVector = transform.right;
+                            break;
+                        //Up
+                        case 4:
+                            directionVector = transform.up;
+                            break;
+                        //Down
+                        case 5:
+                            directionVector = -transform.up;
+                            break;
+                    }
+                }
+
+                positionToSpawn = this.transform.position + directionVector * dist;
+                break;
+            //If skill position is at mouse
+            case 4:
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                if (Physics.Raycast(ray, out hit, 100.0f)) {
+                    positionToSpawn = new Vector3(hit.point.x, 1, hit.point.z);
+                }
+                break;
+        }
+
+        return positionToSpawn;
+    }
+
+    private void InstantiateSkill() {
+        Vector3 startPoint = GetPositionFromMenu(positionChoice1, positionChoice1Direction, skillPositionVector, skillPositionObject, skillPositionDistance);
+        Vector3 targetPoint = GetPositionFromMenu(targetChoice1, targetChoice1Direction, skillTargetVector, skillTargetObject, skillTargetDistance);
+        GameObject skill = Instantiate(prefabSkill, startPoint, Quaternion.identity);
+        skills.Add(skill);
+        targetPositions.Add(targetPoint);
+    }
+}
---
+++ Assets/SkillManagerEditor.cs
@@ -0,0 +1,140 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SkillManager))]
+public class SkillManagerEditor : Editor {
+
+    private SkillManager myTarget;
+    private SerializedObject soTarget;
+
+    //Prefab
+    private SerializedProperty prefabSkill;
+
+    //Trigger
+    private SerializedProperty input;
+    private SerializedProperty minTime;
+    private SerializedProperty maxTime;
+    private SerializedProperty timeBetween;
+    private SerializedProperty cooldownTime;
+
+    //Position
+    public SerializedProperty skillPositionVector;
+    public SerializedProperty skillPositionObject;
+    public SerializedProperty skillPositionDirection;
+    public SerializedProperty skillPositionDistance;
+
+    //Target
+    public SerializedProperty skillTargetVector;
+    public SerializedProperty skillTargetObject;
+    public SerializedProperty skillTargetDirection;
+    public SerializedProperty skillTargetDistance;
+
+    public SerializedProperty skillSpeed;
+
+    private void OnEnable() {
+        myTarget = (SkillManager)target;
+        soTarget = new SerializedObject(target);
+
+        //Prefab
+        prefabSkill = soTarget.FindProperty("prefabSkill");
+
+        //Trigger
+        input = soTarget.FindProperty("input");
+        cooldownTime = soTarget.FindProperty("cooldownTime");
+        minTime = soTarget.FindProperty("minTime");
+        maxTime = soTarget.FindProperty("maxTime");
+        timeBetween = soTarget.FindProperty("timeBetween");
+
+        //Position
+        skillPositionVector = soTarget.FindProperty("skillPositionVector");
+        skillPositionObject = soTarget.FindProperty("skillPositionObject");
+        skillPositionDirection = soTarget.FindProperty("skillPositionDirection");
+        skillPositionDistance = soTarget.FindProperty("skillPositionDistance");
+
+        //Target
+        skillTargetVector = soTarget.FindProperty("skillTargetVector");
+        skillTargetObject = soTarget.FindProperty("skillTargetObject");
+        skillTargetDirection = soTarget.FindProperty("skillTargetDirection");
+        skillTargetDistance = soTarget.FindProperty("skillTargetDistance");
+
+        //Speed
+        skillSpeed = soTarget.FindProperty("skillSpeed");
+    }
+
+    public override void OnInspectorGUI() {
+        //base.OnInspectorGUI();
+        soTarget.Update();
+        EditorGUI.BeginChangeCheck();
+
+        myTarget.currentTab = GUILayout.Toolbar(myTarget.currentTab, new string[] { "Trigger", "Position", "Prefab", "Effect" });
+
+        switch(myTarget.currentTab) {
+            case 0:
+                myTarget.triggerChoice = GUILayout.Toolbar(myTarget.triggerChoice, new string[] { "Player input", "Random", "Continuously" });
+                switch(myTarget.triggerChoice) {
+                    case 0:
+                        EditorGUILayout.LabelField("Trigger by player input");
+                        EditorGUILayout.PropertyField(input);
+                        EditorGUILayout.PropertyField(cooldownTime);
+                        break;
+                    case 1:
+                        EditorGUILayout.LabelField("Trigger at random intervals");
+                        EditorGUILayout.PropertyField(minTime);
+                        EditorGUILayout.PropertyField(maxTime);
+                        break;
+                    case 2:
+                        EditorGUILayout.LabelField("Trigger at specific intervals");
+                        EditorGUILayout.PropertyField(timeBetween);
+                        break;
+                }
+                break;
+            case 1:
+                myTarget.positionChoice = GUILayout.Toolbar(myTarget.positionChoice, new string[] { "Constant position", "Move position" });
+                switch(myTarget.positionChoice) {
+                    case 0:
+                        EditorGUILayout.LabelField("Constant position");
+                        ShowTargetOptions(ref myTarget.positionChoice1, ref myTarget.positionChoice1Direction, ref skillPositionVector, ref skillPositionObject, ref skillPositionDistance);
+                        break;
+                    case 1:
+                        EditorGUILayout.LabelField("Start position");
+                        ShowTargetOptions(ref myTarget.positionChoice1, ref myTarget.positionChoice1Direction, ref skillPositionVector, ref skillPositionObject, ref skillPositionDistance);
+                        EditorGUILayout.LabelField("End position");
+                        ShowTargetOptions(ref myTarget.targetChoice1, ref myTarget.targetChoice1Direction, ref skillTargetVector, ref skillTargetObject, ref skillTargetDistance);
+                        EditorGUILayout.LabelField("Speed");
+                        EditorGUILayout.PropertyField(skillSpeed);
+                        break;
+                }
+                break;
+            case 2:
+                EditorGUILayout.PropertyField(prefabSkill);
+                break;
+            case 3:
+
+                break;
+        }
+
+        if(EditorGUI.EndChangeCheck()) {
+            soTarget.ApplyModifiedProperties();
+            GUI.FocusControl(null);
+        }
+    }
+
+    private void ShowTargetOptions(ref int choice1, ref int choice1dir, ref SerializedProperty vec, ref SerializedProperty obj, ref SerializedProperty dist) {
+        choice1 = GUILayout.Toolbar(choice1, new string[] { "Global", "Local", "GameObject", "Direction", "Mouse" });
+        switch (choice1) {
+            case 0:
+            case 1:
+                EditorGUILayout.PropertyField(vec);
+                break;
+            case 2:
+                EditorGUILayout.PropertyField(obj);
+                break;
+            case 3:
+                choice1dir = GUILayout.Toolbar(choice1dir, new string[] { "Forward", "Backward", "Left", "Right", "Up", "Down" });
+                EditorGUILayout.PropertyField(dist);
+                break;
+        }
+    }
+}
